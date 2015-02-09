6afd426a44d449ea6ce4195b4494d457aa1221a2
Spring.Zhou <spring.zhou@samesamechina.com>
2015-02-09 22:44:18 +0800

    question page and js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,58 @@
-function showRule(){ 
-    var rule=document.getElementById('rule');
-    rule.style.display='block';
- }
-function hideRule(){  
-    var rule=document.getElementById('rule');
-    rule.style.display='none';
- }
 
+;(function($){
+    $(function(){
+
+        $('.showRule').on('click', function(){
+
+            $('.rulebox').css('display', 'block');
+
+        });
+        $('.hideRule').on('click', function(){
+
+            $('.rulebox').css('display', 'none');
+
+        });
+        $('.introduction').animate({"opacity":1}, 5000, function(){
+            
+            $(".introduction").animate({"opacity":0}, 5000, function(){
+                
+                $(".introduction1").animate({"opacity":1}, 5000);
+
+            });
+
+        }); 
+
+
+        $('.lotus3').on('click', function(){
+
+            $('.lotus3 img').attr("src","images/product/lotusb1.png");
+
+
+
+            
+
+        });
+
+        $('.lotus5').on('click', function(){
+
+            $('.lotus5 img').attr("src","images/product/lotusb2.png");
+
+
+
+            
+
+        });
+
+         $('.lotus6').on('click', function(){
+
+            $('.lotus6 img').attr("src","images/product/lotusb3.png");
+
+
+
+            
+
+        });    
+    })
+})(jQuery)
 
 
