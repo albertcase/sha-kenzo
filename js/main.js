628d72f18e31e693011c2be051c9a1d7e879f589
Spring.Zhou <spring.zhou@samesamechina.com>
2015-02-10 11:59:15 +0800

    index page animation

--- js/main.js
+++ js/main.js
@@ -12,53 +12,57 @@
             $('.rulebox').css('display', 'none');
 
         });
-        $('.introduction').animate({"opacity":1}, 5000, function(){
+        $('.introduction').animate({"opacity":1}, 3000, function(){
             
-            $(".introduction").animate({"opacity":0}, 5000, function(){
+            $(".introduction").animate({"opacity":0}, 3000, function(){
                 
-                $(".introduction1").animate({"opacity":1}, 5000);
+                $(".introduction1").animate({"opacity":1}, 3000);
 
             });
 
         }); 
 
-
+        var num = 0;
         $('.lotus3').on('click', function(){
-
+            if($(this).hasClass("hover")) 
+                return false;
+            $(this).addClass("hover");
             $('.lotus3 img').attr("src","images/product/lotusb1.png");
-            $("#home .product1 img").animate({"opacity":1}, 5000);
-            $("#home .product1 .purplebox").animate({"opacity":1}, 5000);
-
-
-
-
-            
+            $("#home .product1 img").css("display", "block").animate({"opacity":1}, 3000);
+            $("#home .product1 .purplebox").animate({"opacity":1}, 3000);
+            num += 1; 
+            if (num==3){
+                $('#nav-right-btn').animate({"opacity":1}, 6000);
+            } 
 
         });
 
         $('.lotus5').on('click', function(){
-
+            if($(this).hasClass("hover")) 
+                return false;
+            $(this).addClass("hover");
             $('.lotus5 img').attr("src","images/product/lotusb2.png");
-            $("#home .product2 img").animate({"opacity":1}, 5000);
-            $("#home .product2 .purplebox").animate({"opacity":1}, 5000);
-
-
-
-            
-
+            $("#home .product2 img").css("display", "block").animate({"opacity":1}, 3000);
+            $("#home .product2 .purplebox").animate({"opacity":1}, 3000);     
+            num += 1; 
+            if (num==3){
+                $('#nav-right-btn').animate({"opacity":1}, 6000);
+            }  
         });
 
          $('.lotus6').on('click', function(){
-
+            if($(this).hasClass("hover")) 
+                return false;
+            $(this).addClass("hover");
             $('.lotus6 img').attr("src","images/product/lotusb3.png");
-            $("#home .product3 img").animate({"opacity":1}, 5000);
-            $("#home .product3 .purplebox").animate({"opacity":1}, 5000);
+            $("#home .product3 img").css("display", "block").animate({"opacity":1}, 3000);
+            $("#home .product3 .purplebox").animate({"opacity":1}, 3000);
+            num += 1; 
+            if (num==3){
+                $('#nav-right-btn').animate({"opacity":1}, 6000);
+            }  
+        }); 
 
-
-
-            
-
-        });    
     })
 })(jQuery)
 
